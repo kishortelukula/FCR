70ad99c52aba2840d2b9798e24838f1c0268bae7
SaikumarGaini <gainisaikumarchinna@gmail.com>
2024-07-22 12:37:43 +0530

    commit

--- FCR_New_Project/src/main/java/com/fcr/services/CaseCreationService.java
+++ FCR_New_Project/src/main/java/com/fcr/services/CaseCreationService.java
@@ -1,6 +1,8 @@
 package com.fcr.services;
 
-
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,28 +13,32 @@ import com.fcr.repository.FcrRepository;
 
 @Service
 public class CaseCreationService {
-	
+
 	@Autowired
 	FcrRepository fcrRepository;
 
 	public String createCase(CaseCreation caseCreation) {
-		String reviewId="FCR-";
-				
-	TaskDetails details	= TaskDetails.builder().reviewId(caseCreation.getReviewId())
-		.division(caseCreation.getDivision())
-		.portfolio(caseCreation.getPortfolio())
-		.assignTo("SR Credit Reviewer")
-		.taskStatus("Created")
-		.build();
-		
-		try{
-		fcrRepository.save(details);
-		return "Case Creation Success";
-		}catch(Exception e) {
+		String fixedString = "FCR-";
+
+		String currentDate = LocalDate.now().format(DateTimeFormatter.ofPattern("ddMMyyyy"));
+
+		Random random = new Random();
+		int randomNumber = random.nextInt(900) + 100;
+
+		String reviewId = fixedString + currentDate + "-" + randomNumber;
+
+		System.out.println(reviewId);
+
+		TaskDetails details = TaskDetails.builder().reviewId(reviewId).division(caseCreation.getDivision())
+				.portfolio(caseCreation.getPortfolio()).assignTo("SR Credit Reviewer").taskStatus("Created").build();
+
+		try {
+			fcrRepository.save(details);
+			return "Case Creation Success";
+		} catch (Exception e) {
 			return "Case Creation Failed";
 		}
-		
-		
+
 	}
 
 }
